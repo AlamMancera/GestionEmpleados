b180547957c42b6236947ca7508f5dc2acc88797
Alam <mczynko@hotmail.com>
2026-02-23 12:28:21 -0600

    feat: Add ReporteEmpleado record

---
+++ ReporteEmpleado.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GestionEmpleados
+{
+    internal record ReporteEmpleado(int Id, string Nombre, string Email, string Rfc, string Nss, decimal Salario, DateTime FechaContratacion, string AntiguedadAnios, EstadoEmpleado Estado)
+    {
+        public string GenerarReporte()
+        {
+            return $"""
+                ****************
+                REPORTE EMPLEADO
+                ****************
+
+                ID:                 {Id}
+                Nombre:             {Nombre}
+                Email:              {Email}
+                RFC:                {Rfc}
+                NSS:                {Nss}
+                Salario:            {Salario:C}
+                Fecha Contratación: {FechaContratacion:dd/MM/yyyy}
+                Antigüedad:         {AntiguedadAnios}
+                Estado:             {Estado}  
+                """;
+        }
+    }
+}
